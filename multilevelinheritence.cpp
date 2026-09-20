807fe3dc9712adc2784243b1fc6ac69a9786eabf
Sana Tasneem Azimudin <sanaazimudin432@gmail.com>
2026-09-20 23:09:54 +0530

    Add files via upload

---
+++ multilevelinheritence.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+class Animal {
+public:
+    void eat() {
+        cout << "Animal is eating." << endl;
+    }
+};
+class Dog : public Animal {
+public:
+    void bark() {
+        cout << "Dog is barking." << endl;
+    }
+};
+class Puppy : public Dog {
+public:
+    void play() {
+        cout << "Puppy is playing." << endl;
+    }
+};
+int main() {
+    Puppy puppy;
+    puppy.eat();
+    puppy.bark();
+    puppy.play();
+    return 0;
+}
