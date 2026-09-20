b9b16b3315b5b989532f3244dbd23d3698ad2ba7
Sana Tasneem Azimudin <sanaazimudin432@gmail.com>
2026-09-21 00:40:23 +0530

    Add files via upload

---
+++ paymentsystem.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+class Payment {
+public:
+    virtual void makePayment() = 0;
+};
+class CreditCard : public Payment {
+public:
+    void makePayment() override {
+        cout << "Payment made using Credit Card." << endl;
+    }
+};
+class UPI : public Payment {
+public:
+    void makePayment() override {
+        cout << "Payment made using UPI." << endl;
+    }
+};
+int main() {
+    CreditCard card;
+    UPI upi;
+    card.makePayment();
+    upi.makePayment();
+    return 0;
+}
