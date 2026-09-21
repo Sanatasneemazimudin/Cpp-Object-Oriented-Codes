40e7373e760e9d6634eec61f0b0ffeed4f901ee2
Sana Tasneem Azimudin <sanaazimudin432@gmail.com>
2026-09-22 00:20:11 +0530

    Add files via upload

---
+++ functionoverriding.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+class Animal {
+public:
+    void sound() {
+        cout << "Animal makes a sound." << endl;
+    }
+};
+class Dog : public Animal {
+public:
+    void sound() {
+        cout << "Dog barks." << endl;
+    }
+};
+int main() {
+    Dog dog;
+    dog.sound();
+    return 0;
+}
